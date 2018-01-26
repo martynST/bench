b2473b415e24ad462976d95e1f972885847e1aea
Martyn <1318921@brunel.ac.uk>
2018-01-26 19:25:22 +0000

    finished basic function of using API with OAuth 2

--- Api/animeApi/animeApi.js
+++ Api/animeApi/animeApi.js
@@ -1,32 +1,66 @@
-const apiUrl = "";
+const apiUrl = "https://anilist.co/api";
+const apiSearch = "https://anilist.co/api/anime/search"
+const apiAnime = "https://anilist.co/api/anime"
 
-let onSuccess = (response) => {
-  return response.json();
-};
-let onError = (error) => {
-  console.log(error);
-};
+let data = {};
 
-async function getToken() {
-  let request = fetch("https://anilist.co/api/auth/access_token?grant_type=client_credentials&client_id=martynt-sfop8&client_secret=LJd4O13TQZeBZi79Gb05", {
-    method: "POST",
-    qs:
-      {
+export async function requestToken() {
 
-      },
-    headers:
-      {
-
-      }
+  let promise = fetch("https://anilist.co/api/auth/access_token?grant_type=client_credentials&client_id=martynt-sfop8&client_secret=LJd4O13TQZeBZi79Gb05", {
+    method: "POST"
   });
 
-  let result = await request;
-  let promise = result.json();
-  let data = await promise;
-  console.log(data);
+  let result = await promise;
+  let promise2 = result.json();
+  data = await promise2;
   return data;
 }
 
-export function requestToken() {
-  console.log(getToken());
+export async function getAnimeSearch(animeName) {
+  let promise = fetch(`${apiSearch}/${animeName}`, {
+    method: "GET",
+    "headers": {
+      Authorization: `${data.token_type} ${data.access_token}`
+    }
+  })
+
+  let result = await promise;
+  let promise2 = result.json();
+  let result2 = await promise2;  
+  return result2;
+}
+
+export async function getAnime(ids) {
+  let promise = [];
+  for (let i = 0; i < ids.length; i++) {
+    promise.push(fetch(`${apiAnime}/${ids[i]}`, {
+      method: "GET",
+      "headers": {
+        Authorization: `${data.token_type} ${data.access_token}`
+      }
+    }))
+  }
+
+  let result = await Promise.all(promise);
+  let promise2 = [];
+  for (let i = 0; i < ids.length; i++) {
+    promise2.push(
+      result[i].json()
+    );
+  }
+
+  let result2 = await Promise.all(promise2);
+
+  console.log(result2)
+
+  let anime = []
+  for (let i = 0; i < ids.length; i++) {
+    anime[i] = {
+      id: `${result2[i].id}`,
+      title: `${result2[i].title_english}`,
+      status: `${result2[i].airing_status}`,
+      episodes: `${result2[i].duration}`
+    }
+  }
+  return anime
 }
--- Api/animeApi/index.js
+++ Api/animeApi/index.js
@@ -1,8 +1,58 @@
-import {requestToken} from "./animeApi.js"
+import { requestToken, getAnimeSearch, getAnime } from "./animeApi.js"
 
-function getToken() {
-  console.log(requestToken());
+const shows = [18679, 9253, 97986, 136];
+
+async function getToken() {
+  await requestToken();  
+  addShowsToTable();
 }
 
+async function search() {
+  let animeName = document.getElementById('animeName').value;
+  let searchResults = await getAnimeSearch(animeName);
+
+  let searchTable = "<table><tr><th>title</th><th>status</th><th>episodes</th></tr>";
+  for (let i = 0; i < searchResults.length; i++) {
+    searchTable += `<tr id='${searchResults[i].id}' onclick='addToList(${searchResults[i].id})'><td>${searchResults[i].title_english}</td><td>${searchResults[i].airing_status}</td><td>${searchResults[i].duration}</td></tr>`;
+  }
+  searchTable += "</table>";
+
+  document.getElementById('searchResults').innerHTML = "<hr>" + searchTable;
+}
+
+async function addShowsToTable() {
+  let showData = await getAnime(shows);
+
+
+  for (let i = 0; i < shows.length; i++) {
+    addOneToTable(showData[i]);
+  }
+}
+
+async function addToList(id) {
+  shows.push(id);  
+  document.getElementById('searchResults').innerHTML = "";  
+  let showDetails = await getAnime([id])
+  addOneToTable(showDetails[0]);
+}
+
+function addOneToTable(animeShow) {
+
+  let table = document.getElementById("showsTable");
+  let row, cellTitle, cellStatus, cellEpisodes;
+  row = table.insertRow(table.rows.length);
+
+  cellTitle = row.insertCell(0);
+  cellStatus = row.insertCell(1);
+  cellEpisodes = row.insertCell(2);
+
+  cellTitle.innerHTML = animeShow.title;
+  cellStatus.innerHTML = animeShow.status;
+  cellEpisodes.innerHTML = animeShow.episodes;
+
+}
 
 window.getToken = getToken;
+window.search = search;
+window.addShowsToTable = addShowsToTable;
+window.addToList = addToList;
